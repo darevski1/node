c7b740c8d18a9668b28b2b9f0227334b4f05dd34
Darko <darevski1@gmail.com>
2020-11-17 21:52:40 +0100

    classs number one

---
+++ c01/index.js
@@ -0,0 +1,28 @@
+
+console.time("Pero")
+console.log("Hi 5")
+
+
+console.error("Greska")
+
+// se koristi poveke za pecatenje na objekti
+let obj = { ime: "Pero", prezime: "Perovski" }
+console.dir(obj)
+
+
+//  za pecatenje tabeli
+let tbl = [
+    { ime: "Pero", prezime: "Perovski" },
+    { ime: "Pero", prezime: "Perovski" },
+    { ime: "Pero", prezime: "Perovski" },
+    { ime: "Pero", prezime: "Perovski" },
+    { ime: "Pero", prezime: "Perovski" },
+    { ime: "Pero", prezime: "Perovski" }
+]
+console.table(tbl)
+
+console.timeEnd("Pero")
+
+
+// Razdeli i vlajder
+// rubber duck deguging
